624347c24f53fc05c1bdaf478d0f547eefa13869
Salem <salem@catchron.com>
2022-05-04 18:54:52 +0300

    added screenshots and docasaurus files

---
+++ CatChron/.docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require('/home/kpetkov/github/catchron-kb/CatChron/node_modules/infima/dist/css/default/default.css'),
+  require('/home/kpetkov/github/catchron-kb/CatChron/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
+  require('/home/kpetkov/github/catchron-kb/CatChron/node_modules/@docusaurus/theme-classic/lib/admonitions.css'),
+  require('/home/kpetkov/github/catchron-kb/CatChron/src/css/custom.css'),
+];
